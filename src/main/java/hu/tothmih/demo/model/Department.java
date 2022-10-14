adc33246f30f0ebee7f4bec3a5761c28bf205649
sze1totmih <sze1totmih@vasvari.org>
2022-10-14 14:38:03 +0200

    department is complete

---
+++ src/main/java/hu/tothmih/demo/model/Department.java
@@ -0,0 +1,34 @@
+package hu.tothmih.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @Column(name = "id",nullable = false)
+    Long id;
+
+    @Column(name = "departmentname",nullable = false,unique = true)
+    String departmentName;
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
+}
