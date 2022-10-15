ffb0b48a4930d817ebb67811368ec578edd557ae
sze1totmih <sze1totmih@vasvari.org>
2022-10-15 13:53:12 +0200

    Controller start

---
+++ src/main/java/hu/tothmih/demo/controller/CoordinatorAPIController.java
@@ -0,0 +1,35 @@
+package hu.tothmih.demo.controller;
+
+import hu.tothmih.demo.model.Coordinator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CoordinatorAPIController {
+    @Autowired
+    CoordinatorService service;
+
+    @GetMapping(value = "/coordinator",name = "List of all coordinators.")
+    public List<Coordinator> getAllCoordinators(){
+        return service.getAllCoordinator();
+    }
+
+    @PostMapping(value = "/coordinator",name = "Save coordinator")
+    private long saveCoordinator(@RequestBody Coordinator coordinator){
+        service.saveOrUpdate(coordinator);
+        return coordinator;
+    }
+
+    @PutMapping(value = "/api/subject", name ="Update coordinator")
+    private Coordinator update(@RequestBody Coordinator coordinator){
+        service.saveOrUpdate(Coordinator);
+        return coordinator;
+    }
+
+    @DeleteMapping(value = "/api/coordinator/{coordinatorid}", name ="Delete coordinator")
+    private void deleteCoordinator(@PathVariable("coordinatorid") Long coordinatorid){
+        service.delete(coordinatorid);
+    }
+}
---
+++ src/main/java/hu/tothmih/demo/controller/CoordinatorController.java
@@ -0,0 +1,22 @@
+package hu.tothmih.demo.controller;
+
+import hu.tothmih.demo.model.Coordinator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CoordinatorController {
+    @Autowired
+    CoordinatorService service;
+
+    public ModelAndView showCoordinatorList(){
+        List<Coordinator> coordinators= service.getAllCoordinator();
+        ModelAndView mav=new ModelAndView("coordinators/index");
+        mav.addAllObjects("coordinators",coordinators);
+
+        return mav;
+    }
+}
